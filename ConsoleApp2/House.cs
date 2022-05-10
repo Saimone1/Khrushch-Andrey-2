1a3f8b515a6b53db42b68851f0360da764a50ffc
Saimone1 <csgo.reserv@gmail.com>
2022-05-10 15:50:17 +0300

    Add Laba2
    Signed-off-by: Saimone1 <csgo.reserv@gmail.com>

---
+++ ConsoleApp2/House.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class House
+    {
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Кiлькiсть вiкон у будинку - " + Windows.Num);
+            Console.WriteLine("Кiлькiсть дверей у будинку - " + Doors.Num);
+        }
+        public void Add(Components component)
+        {
+            component.Add();
+            Console.WriteLine("Доданий об'єкт " + component.ToString());    
+        }
+    }
+    public abstract class Components
+    {
+        public abstract void Add();
+    }
+    public class Windows : Components
+    {
+        public static int Num;
+
+        public override void Add()
+        {
+            Num++;
+        }
+    }
+    public class Doors : Components
+    {
+        public static int Num = 0;
+        private bool Lock1 = false;
+
+        public override void Add()
+        {
+            Num++;
+        }
+
+        public bool Lock()
+        {
+            string answ;
+            if (!this.Lock1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Дверi " + "вiдчиненi");
+                Console.WriteLine("Зачинити дверi?");
+                answ = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Дверi зачиненi");
+                Console.WriteLine("Вiдчинити дверi?");
+                answ = Console.ReadLine();
+
+            }
+            if (answ.ToUpper() == "YES")
+            {
+                Console.WriteLine("Дверi зачинились/вiдчинились!");
+                return this.Lock1 = !this.Lock1;
+            }
+            else
+            {
+                Console.WriteLine("");
+                return this.Lock1;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House();
+
+            Windows window1 = new Windows();
+            Windows window2 = new Windows();
+            Windows window3 = new Windows();
+            house.Add(window1);
+            house.Add(window2);
+
+            house.Print();
+
+            Doors maindoor = new Doors();
+            Doors door1 = new Doors();
+            house.Add(maindoor);
+            house.Add(door1);
+            house.Add(window3);
+
+            house.Print();
+
+            maindoor.Lock();
+            maindoor.Lock();
+
+            Console.WriteLine("Наступні двері:");
+            door1.Lock();
+            door1.Lock();
+        }
+    }
+}
